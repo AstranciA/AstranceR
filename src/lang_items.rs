09f839c02038283455715f9f77b7fdd42be52b75
ZreXoc <zeexoc@outlook.com>
2025-03-02 14:49:30 +0800

    feat(deps): update dependencies

--- src/lang_items.rs
+++ src/lang_items.rs
@@ -7,10 +7,10 @@ fn panic_handler(info: &PanicInfo) -> ! {
             "Panicked at {}:{}: {}",
             location.file(),
             location.line(),
-            info.message().unwrap()
+            info.message()
         );
     } else {
-        println!("Panicked: {}", info.message().unwrap());
+        println!("Panicked: {}", info.message());
     }
     loop {}
 }
